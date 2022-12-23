3aa05b9362985eaa714a1e97cc3076510b80431f
amithkb21 <121281179+amithkb21@users.noreply.github.com>
2022-12-23 17:58:45 +0530

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -2,9 +2,7 @@
 
 git pull origin master
 
-find /var/www/html/wl2/dev -name "*.env" -exec sed -i 's/APP_ENV=LOCAL/APP_ENV=DEV/g' {} \; 
-
-find /var/www/html/wl2/testing -name "*.env" -exec sed -i 's/APP_ENV=LOCAL/APP_ENV=TEST/g' {} \;
+find /var/www/html/wl2/dev -name "*.env" -exec sed -i 's/APP_ENV=LOCAL/APP_ENV=DEV/g' {} \; && find /var/www/html/wl2/testing -name "*.env" -exec sed -i 's/APP_ENV=LOCAL/APP_ENV=TEST/g' {} \;
 
 
 
