176bbad40e2f1f996fbd49b4f4cf0e0fea3fdcd3
amit <amit123@gmail.com>
2022-12-26 19:21:29 +0000

    all

--- task.sh
+++ task.sh
@@ -18,8 +18,11 @@ then
 else
 	echo " new commit id found "
 	
-	git checkout test
-	git merge dev
+	
+	find /mnt/dev/ -name "*.env" -exec sed -i 's/APP_ENV=LOCAL/APP_ENV=DEV/g' {} \;
+
+	find /mnt/testing/ -name "*.env" -exec sed -i 's/APP_ENV=LOCAL/APP_ENV=TEST/g' {} \;
+
 
 fi
 
