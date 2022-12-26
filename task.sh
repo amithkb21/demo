b940d097ff331e84e4ffae97740a1982e1354f3b
root <root@ip-172-31-47-194.us-east-2.compute.internal>
2022-12-26 18:10:49 +0000

    task file add

---
+++ task.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+origin="origin"
+branch="master"
+commit_id=$(git log -n 1 --pretty=format:%H "$origin/$branch")
+
+echo $commit_id 
+
+commit_old=$(git log -n 2 --pretty=format:%H "$origin/$branch" | tail -1 )
+echo $commit_old
+
+
+if [ $commit_id == $commit_old ];
+
+then 
+	echo " same id "
+else
+	echo " new commit id "
+fi
+
