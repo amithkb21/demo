8b78e74d9049d36a402da710b8c7cb6acc7651e0
amithkb21 <121281179+amithkb21@users.noreply.github.com>
2022-12-27 11:26:32 +0530

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -3,11 +3,11 @@
 origin="origin"
 branch="master"
 new_commit_id=$(git log -n 1 --pretty=format:%H "$origin/$branch")
-echo $commit_id_latest
+echo $new_commit_id
 
 old_commit_id=$(</home/amitayare/file1.txt) 
 
-echo "$value"  
+echo "$old_commit_id"  
 
 if [ $new_commit_id == $old_commit_id ];
 
@@ -15,9 +15,6 @@ then
 	echo " NO new commits "
 else
 	echo " new commit id is FOUND "
-
-
-
 
 commit_save=$(git log -n 1 --pretty=format:%H "$origin/$branch")
 echo $commit_save > /home/amitayare/file1.txt 
